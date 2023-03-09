6947f9ca91dfcfcb434f4fce6fa78a83a67b6d5b
SeanHarrisQA <sharris3@qa.com>
2023-03-09 16:24:45 +0000

    Complete conditionals and remove source code from repo

--- src/conditionals/Conditionals.java
+++ src/conditionals/Conditionals.java
@@ -2,4 +2,22 @@ package conditionals;
 
 public class Conditionals {
 
+	public static void main(String[] args) {
+		// Code to test my method here
+		System.out.println(conditional(1, 2, true));
+		System.out.println(conditional(3, 3, false));
+		System.out.println(conditional(1, 1, true));
+		System.out.println(conditional(2, 1, true));
+		System.out.println(conditional(2, 2, true));
+	}
+
+	public static int conditional(int a, int b, boolean p) {
+		if ((a % 2 == 0) || (b % 2 == 0)) {
+			return 0;
+		} else if (p) {
+			return a + b;
+		} else {
+			return a * b;
+		}
+	}
 }
