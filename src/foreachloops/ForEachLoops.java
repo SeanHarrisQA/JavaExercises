7b590cf347b3a0d82870617a384794398de31d81
SeanHarrisQA <Sharris3@qa.com>
2023-03-14 12:34:20 +0000

    Complete ForEachLoops.java

--- src/foreachloops/ForEachLoops.java
+++ src/foreachloops/ForEachLoops.java
@@ -7,5 +7,26 @@ public class ForEachLoops {
 		for (String treat : xmasTreats) {
 			System.out.println(treat);
 		}
+
+		int[] oneToTwenty = new int[20];
+		for (int i = 0; i < oneToTwenty.length; i++) {
+			oneToTwenty[i] = i + 1;
+		}
+
+		for (int number : oneToTwenty) {
+			if (checkEven(number)) {
+				System.out.println(Math.pow(number, 3));
+			} else {
+				System.out.println(Math.pow(number, 2));
+			}
+		}
+	}
+
+	public static boolean checkEven(int number) {
+		if (number % 2 == 0) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 }
