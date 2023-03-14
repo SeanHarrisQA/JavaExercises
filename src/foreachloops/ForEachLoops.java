d75e548ac084d4e5a31bee74c25937f6170297cf
SeanHarrisQA <Sharris3@qa.com>
2023-03-14 12:12:39 +0000

    Create ForEachLoops.java

---
+++ src/foreachloops/ForEachLoops.java
@@ -0,0 +1,11 @@
+package foreachloops;
+
+public class ForEachLoops {
+
+	public static void main(String[] args) {
+		String[] xmasTreats = { "Chocolate Orange", "Turkey", "Potatoes", "Sprouts", "Cranberry Sauce" };
+		for (String treat : xmasTreats) {
+			System.out.println(treat);
+		}
+	}
+}
